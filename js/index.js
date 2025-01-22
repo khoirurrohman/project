83bb30f2a2a59d125f1f1c67ae431d5a13712f9d
khoirurrohman <rohmanjr027@gmail.com>
2025-01-22 11:13:34 +0700

    belajar js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// for(let i =1; i<= 36; i++) {
+//     console.log( 4 * i +2)
+// }
+// for(let i = 1; i <= 10; i++) {
+//     if(i % 2 == 0 ){
+//         console.log(i)
+//     }
+// }
+
+// var fruits = [ "apel", "strawbery", "mangga", "yudi", "adi"]
+// for(var i =0; i <buah.length; i++){
+// console.log(buah[i])
+// }
+// console.log(buah.length)
+
+//menggunakan foreach
+// fruits.forEach(fruit => {
+//     console.log(fruit)
+// })
