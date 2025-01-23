0ac8c62b3eab5a0cf11bef79ae2122165f0d5ab8
khoirurrohman <rohmanjr027@gmail.com>
2025-01-23 21:06:51 +0700

    masih belajar js

--- js/index.js
+++ js/index.js
@@ -13,7 +13,72 @@
 // }
 // console.log(buah.length)
 
-//menggunakan foreach
+// //menggunakan foreach
 // fruits.forEach(fruit => {
 //     console.log(fruit)
+// })
+// var users = [
+//     {
+//         name: "asep",
+//         password: "123",
+//     },
+//     {
+//         name: "adi",
+//         password: "1234",
+//     },
+//     {
+//         name: "wahyudi",
+//         password: "1234",
+//     },
+//     {
+//         name: "alvin",
+//         password: "1234",
+//     },
+//     {
+//         name: "aydi",
+//         password: "1234",
+//     },
+//     {
+//         name: "faisal",
+//         password: "1234",
+//     },
+//     {
+//         name: "holqi",
+//         password: "1234",
+//     }
+// ]
+// users.forEach(user =>{
+//     console.log(user.name)
+//     console.log(user.password)
+//     console.log(users[3]["name"])
+// })
+
+
+
+
+// function ambilDigit(bilangan) {
+//     const digitPertama = Math.floor(bilangan / 10);
+//     const digitKedua = bilangan % 10;              
+//     return { digitPertama, digitKedua };
+// }
+
+
+// const bilangan = [35];
+// bilangan.forEach(angka => {
+//     const { digitPertama, digitKedua } = ambilDigit(angka);
+//     console.log(`Bilangan: ${angka}, Digit pertama: ${digitPertama}, Digit kedua: ${digitKedua}`);
+// });
+
+// var users = [
+//         {
+//             kuning: "adi suka",
+            
+//         },
+//         {
+//             unggu: "adi tidak suka",
+            
+//         }
+// ]
+// users.forEach(user =>{
+//     console.log(user.unggu)
 // })
